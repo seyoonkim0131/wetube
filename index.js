c66401e22d5ecb691049e76a71d721b9c090e48d
seyoonkim <rlatpdbs@gmail.com>
2019-09-20 15:48:47 +0900

    #2.6 Express Core: Middlewares

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ const PORT = 4000
 const handleListening = () => {
     console.log(`Listening on: http:://localhost:${PORT}`);
 }
+const handleHome = (req, res) => res.send("Hello from my ass")
+const handleProfile = (req, res) => res.send("You are on my profile")
+const betweenHome = (req, res, next) => {
+    console.log("I'm between")
+    next()
+}
+
+app.use(betweenHome)
 
 app.get('/', (req, res) => res.send('Hello World:)'))
+app.get('/profile', handleProfile)
 
 app.listen(PORT, handleListening)
